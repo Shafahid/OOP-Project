e89d231a4a9c94ad10d84666ae98ec52316b7929
Shafahid <shafahidrahman@gmail.com>
2025-01-28 13:18:36 +0600

    fourth

--- src/main/java/com/example/disaster/ChatServer.java
+++ src/main/java/com/example/disaster/ChatServer.java
@@ -8,12 +8,12 @@ import java.util.List;
 
 
 
-class ClientUser {
+class User {
     String name;
     Socket socket;
     BufferedWriter writer;
 
-    public ClientUser(String name, Socket socket, BufferedWriter writer) {
+    public User(String name, Socket socket, BufferedWriter writer) {
         this.name = name;
         this.socket = socket;
         this.writer = writer;
@@ -35,8 +35,8 @@ class ClientUser {
 public class ChatServer {
 
     static int PORT = 6666; // Server port
-    static  List<ClientUser> clients = new ArrayList<>(); // List of connected clients
-    static ClientUser adminUser;
+    static  List<User> clients = new ArrayList<>(); // List of connected clients
+    static User adminUser;
 
 
 
@@ -61,11 +61,11 @@ public class ChatServer {
 
                 // Check if this is the admin
                 if (clientName.equalsIgnoreCase("Admin")) {
-                    adminUser = new ClientUser(clientName, socket, writer);
+                    adminUser = new User(clientName, socket, writer);
                     System.out.println("Admin is connected.");
                 } else {
                     synchronized (clients) {
-                        clients.add(new ClientUser(clientName, socket, writer));
+                        clients.add(new User(clientName, socket, writer));
                     }
 
 
@@ -84,7 +84,7 @@ public class ChatServer {
                         if (parts.length == 2) {
                             String targetClient = parts[0].trim();
                             String adminMessage = parts[1].trim();
-                            sendMessageToClient(targetClient, "Admin: " + adminMessage);
+                            sendMessage(targetClient, "Admin: " + adminMessage);
                         }
                     } else {
 
@@ -113,9 +113,9 @@ public class ChatServer {
             }
         }
 
-        private void sendMessageToClient(String clientName, String message) {
+        private void sendMessage(String clientName, String message) {
             synchronized (clients) {
-                for (ClientUser client : clients) {
+                for (User client : clients) {
                     if (client.getName().equalsIgnoreCase(clientName)) {
                         try {
                             client.getWriter().write(message + "\n");
